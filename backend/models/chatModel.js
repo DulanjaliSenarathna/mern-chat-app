723109ac67001709e8ac35b8c3902da400dfd3d7
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-07-10 08:20:18 +0530

    Defining Schema and Models with mongoose done

---
+++ backend/models/chatModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const chatModel = mongoose.Schema(
+    {
+        chatName:{type:String, trim:true},
+        isGroupChat: {type: Boolean, default:false},
+        users:[{
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"User"
+        }],
+        latestMessaage:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"User"
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+const Chat = mongoose.model("Chat",chatModel)
+
+module.exports = Chat;
