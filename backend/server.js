5623bc8af9e02c2f0be5b8054e10455d88f81c8d
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-07-17 20:48:02 +0530

    Connect Mongo DB to Node JS

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv')
 const { chats } = require('./data/data');
+const connectDB = require('./config/db');
+const colors = require('colors')
 
 const app = express();
 dotenv.config();
+connectDB();
 
 app.get('/',(req,res)=>{
     res.send('API is running now');
@@ -21,4 +24,4 @@ app.get('/api/chat/:id',(req,res)=>{
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`.yellow.bold));
